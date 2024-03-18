f5da38c9a5b9a99e99597ca3dcdf011385d6359e
Codiku <111140836+codiku-dev@users.noreply.github.com>
2024-03-18 12:09:46 +0100

    Update movie-api.js

--- src/assets/API/movie-api.js
+++ src/assets/API/movie-api.js
@@ -1,4 +1,5 @@
 // rlebhar : extraire clé d'api et base url dans un /config/config.js
+// rlebhar : Ne pas oublier de try catch les call et de retourner un résultat cohérent en cas d'erreur.
 export const MovieApi = {
         fetchPopularMovies: async () => {
                 const response = await fetch('https://api.themoviedb.org/3/tv/popular?api_key=6f06b3d4a05561645d9473bb77e9b4bc')
