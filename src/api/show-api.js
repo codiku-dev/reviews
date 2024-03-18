96794854c971d9160ef4822007086e555994f087
Codiku <111140836+codiku-dev@users.noreply.github.com>
2024-03-18 12:19:03 +0100

    Update show-api.js

--- src/api/show-api.js
+++ src/api/show-api.js
@@ -2,6 +2,7 @@ const API_KEY = 'e8794088592c89d66928f6c4c14e71a2'; // todo: add in .env file
 
 // rlebhar : Ajouter try catch pour sécuriser si l'api casse
 // rlebhar : Externaliser aussi https://api.themoviedb.org
+// rlebhar : Ajouter try catch et retourner un résultat cohérent en cas d'erreur
 export async function fetchShows() {
     const response = await fetch(`https://api.themoviedb.org/3/tv/popular?api_key=${API_KEY}`);
     const data = await response.json();
